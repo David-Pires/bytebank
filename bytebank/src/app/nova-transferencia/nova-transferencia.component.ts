e559af3441a51fcd49fec5b648ac8ad7151bc982
David Pires <developer.david@outlook.com>
2021-12-20 14:40:26 -0300

    Add ngModule for the template and CSS Classes

--- bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
+++ bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,10 +1,18 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-nova-transferencia',
   templateUrl: './nova-transferencia.component.html',
-  styleUrls: ['./nova-transferencia.component.scss']
+  styleUrls: ['./nova-transferencia.component.scss'],
 })
 export class NovaTransferenciaComponent {
+  valor: number;
+  destino: number;
+  transferir() {
+    console.log('Solicitado nova transferencia');
+    console.log('valor: ', this.valor);
+    console.log('destino: ', this.destino);
 
+
+  }
 }
